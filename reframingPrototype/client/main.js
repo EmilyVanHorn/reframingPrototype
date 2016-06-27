49217ab3105722946e36b9985fb836835baeecc3
Emily <ejv1003@sru.edu>
2016-06-27 13:43:01 -0400

    add theme metadata

--- reframingPrototype/client/main.js
+++ reframingPrototype/client/main.js
@@ -1,9 +1,34 @@
+var metaIndex = 0;
+var meta=[
+    {
+        active: 10,
+        total: 100,
+        prob: 15
+    },
+    {
+        active: 38,
+        total: 100,
+        prob: 80
+    },
+    {
+        active: 62,
+        total: 100,
+        prob: 20
+    },
+    ];
+
+
+
 Template.ideasList.helpers({
     idea: Ideas.find()
 });
 
 Template.themesList.helpers({
     theme: Themes.find()
+});
+
+Template.theme_item.helpers({
+    
 });
 
 Template.idea_item.helpers({
@@ -13,17 +38,23 @@ Template.input.events({
     submit: function(e){
         e.preventDefault();
         value = e.target.firstElementChild.value;
-        Themes.insert({
-            content: value
-        });
         e.target.firstElementChild.value = "";
+
+        Themes.insert({
+            content: value,
+            active: meta[metaIndex].active,
+            total: meta[metaIndex].total,
+            prob: meta[metaIndex].prob
+        });
+        
+        if (metaIndex == (meta.length - 1)){
+            metaIndex = 0;       
+        }
+        else{
+            metaIndex++;   
+        }
+        
+        
+        
     }
 });
-
- function done(){
-            //save themes
-            //write to file?
-            //themesData = Themes.find().fetch();
-            //console.log(themesData);
-            //alert('hi');
-        }
